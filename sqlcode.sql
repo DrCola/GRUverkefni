721af1101402a8a8203c5fcf3a6b26f28d930b26
mundi95 <kristmundurari@gmail.com>
2016-05-11 11:43:01 +0000

    sql

--- sqlcode.sql
+++ sqlcode.sql
@@ -1,18 +1,40 @@
-create database 1208982809_gru_verkefni
-
-create table notendur
+CREATE TABLE members 
 (
-    id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    notendanafn varchar(128) NOT NULL UNIQUE,
-    lykilorð varchar(128) NOT NULL,
-    netfang varchar(128) NOT NULL
+    id INTEGER NOT NULL PRIMARY KEY,
+    name VARCHAR(128) NOT NULL,
+    CONSTRAINT name UNIQUE (name),
+    password VARCHAR(128) NOT NULL,
+    email VARCHAR(128) NOT NULL,
+    CONSTRAINT email UNIQUE (email)
 );
 
-CREATE table myndir/texti
+CREATE TABLE forums
 (
-    id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    notandi varchar(128) FOREIGN KEY REFERENCES notendur(notendanafn),
-    flokkur_id varchar(128) FOREIGN KEY REFERENCES flokkar(id),
-    texti varchar(1500) NOT NULL,
-    myndir varchar(128)
+    id INTEGER NOT NULL PRIMARY KEY,
+    name VARCHAR(128) NOT NULL,
+    CONSTRAINT forum_name UNIQUE ( name ) 
 );
+
+CREATE TABLE threads 
+(
+    id INTEGER NOT NULL PRIMARY KEY,
+    name VARCHAR(128) NOT NULL UNIQUE,
+    forum_id INTEGER NOT NULL,
+    starter INTEGER NOT NULL,
+    FOREIGN KEY (forum_id) REFERENCES forums(id),
+    FOREIGN KEY (starter) REFERENCES members(id)
+);
+
+CREATE TABLE posts 
+(
+    id INTEGER NOT NULL PRIMARY KEY,
+    name VARCHAR(128) NULL,
+    thread_id INTEGER NOT NULL,
+    reply_to INTEGER NULL,
+    posted_by INTEGER NOT NULL,
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    revised TIMESTAMP NULL CHECK (revised >= created),
+    post TEXT NOT NULL,
+    FOREIGN KEY (thread_id) REFERENCES threads(id),
+    FOREIGN KEY (posted_by) REFERENCES members(id)
+ );
